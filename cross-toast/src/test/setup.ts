8b3308206627876d1d1ca85f32ff3727f856bddf
Akala Emmanuel <adekite@Adekites-MacBook-Air.local>
2025-03-16 09:18:46 +0000

    chore: removed duplicate folder

--- cross-toast/src/test/setup.ts
+++
@@ -1,6 +0,0 @@
-// tests/setup.ts
-import '@testing-library/jest-dom/vitest';
-import { expect } from 'vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers);
